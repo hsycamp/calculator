f0a9f3c600972ad1c6d36d9d10ffe0733a85c200
hsycamp <hsycamp@gmail.com>
2019-03-30 19:49:58 +0900

    사칙연산 기호에 따라 계산 가능하도록 구현

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,18 +2,29 @@ import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
+
 		System.out.println("첫번째 입력값: ");
 		int first = scanner.nextInt();
-		System.out.println(first);
-		System.out.println("첫번째 입력값: ");
-		int second = scanner.nextInt();
-		System.out.println(second);
 
-		System.out.println("덧셈" + (first + second));
-		System.out.println("뺄셈" + (first - second));
-		System.out.println("곱셈" + (first * second));
-		System.out.println("나눗셈" + (first / second));
+		System.out.println("연산기호를 입력하세요: ");
+		String symbol = scanner.next();
+
+		System.out.println("두번째 입력값: ");
+		int second = scanner.nextInt();
+
+		if (symbol.equals("+")) {
+			System.out.println(first + second);
+		} else if (symbol.equals("-")) {
+			System.out.println(first - second);
+		} else if (symbol.equals("*")) {
+			System.out.println(first * second);
+		} else if (symbol.equals("/")) {
+			System.out.println(first / second);
+		} else {
+			System.out.println("사칙연산 기호가 아닙니다.");
+		}
 
 		scanner.close();
 	}
