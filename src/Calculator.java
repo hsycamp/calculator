c290d451fc526dad93bd1b760ee41db117c17d41
hsycamp <hsycamp@gmail.com>
2019-03-30 18:54:07 +0900

    2 개의 깂을 입력 받아 사칙연산 계산

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println(3+4);
-	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("첫번째 입력값: ");
+		int first = scanner.nextInt();
+		System.out.println(first);
+		System.out.println("첫번째 입력값: ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+
+		System.out.println("덧셈" + (first + second));
+		System.out.println("뺄셈" + (first - second));
+		System.out.println("곱셈" + (first * second));
+		System.out.println("나눗셈" + (first / second));
+
+		scanner.close();
 	}
 
 }
